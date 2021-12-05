cd7c5f9484f8753ec8235d276144eca0ca17380b
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-12-05 03:03:11 +0100

    Stars 3 & 4.

---
+++ 3/advent3.lisp
@@ -0,0 +1,28 @@
+(defun get-pos (&rest cs)
+  (let ((hpos 0) (depth 0))
+    (dolist (c cs)
+      (let ((id (first c))
+	    (v (rest c)))
+	(ecase id
+	  (down (incf depth v))
+	  (forward (incf hpos v))
+	  (up (decf depth v)))))
+    (* hpos depth)))
+
+(defun test ()
+  (assert (= (apply #'get-pos '((forward . 5)
+				(down . 5)
+				(forward . 8)
+				(up . 3)
+				(down . 8)
+				(forward . 2))) 150))
+  
+  (with-open-file (in "~/Projects/advent/3/input")
+    (labels ((read-cs (out)
+	       (if in
+		   (let ((id (read in nil)))
+		     (if id
+			 (read-cs (cons (cons id (read in)) out))
+			 (nreverse out))))))
+      (let ((ms (read-cs nil)))
+	(format t "answer: ~a ~a~%" (length ms) (apply #'get-pos ms))))))
